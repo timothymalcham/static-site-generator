f0b398c9abc9046905edceb4f03b0c6b245122d4
Timothy Malcham <timothymalcham@gmail.com>
2024-08-18 16:16:23 +0100

    add markdown images and link utils

--- src/test_utils.py
+++ src/test_utils.py
@@ -33,5 +33,66 @@ class TestTextNode(unittest.TestCase):
         self.assertEqual(html_node.children, expected.children)
         self.assertEqual(html_node.props, expected.props)
 
+    def test_split_nodes_delimiter_code(self):
+        text_type_text = "text"
+        text_type_code = "code"
+        node = TextNode("This is text with a `code block` word", text_type_text)
+        new_nodes = Utils.split_nodes_delimiter([node], "`", text_type_code)
+        expected = [
+            TextNode("This is text with a ", text_type_text, None),
+            TextNode("code block", text_type_code, None),
+            TextNode(" word", text_type_text, None)
+        ]
+        self.assertEqual(new_nodes, expected)
+
+    def test_split_nodes_delimiter_bold(self):
+        text_type_text = "text"
+        text_type_bold = "bold"
+        node = TextNode("This is text with a **bold block** word", text_type_text)
+        new_nodes = Utils.split_nodes_delimiter([node], "**", text_type_bold)
+        expected = [
+            TextNode("This is text with a ", text_type_text, None),
+            TextNode("bold block", text_type_bold, None),
+            TextNode(" word", text_type_text, None)
+        ]
+        self.assertEqual(new_nodes, expected)
+
+    def test_split_nodes_delimiter_italic(self):
+        text_type_text = "text"
+        text_type_italic = "italic"
+        node = TextNode("This is text with a *italic block* word", text_type_text)
+        new_nodes = Utils.split_nodes_delimiter([node], "*", text_type_italic)
+        expected = [
+            TextNode("This is text with a ", text_type_text, None),
+            TextNode("italic block", text_type_italic, None),
+            TextNode(" word", text_type_text, None)
+        ]
+        self.assertEqual(new_nodes, expected)
+
+    def test_extract_markdown_images(self):
+        text = "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"
+        images = Utils.extract_markdown_images(text)
+        expected = [("rick roll", "https://i.imgur.com/aKaOqIh.gif"), ("obi wan", "https://i.imgur.com/fJRm4Vk.jpeg")]
+        self.assertEqual(images, expected)
+
+    def test_extract_markdown_images_no_valid_images(self):
+        text = "This is text with a [rick roll](https://i.imgur.com/aKaOqIh.gif) and (https://i.imgur.com/fJRm4Vk.jpeg)"
+        images = Utils.extract_markdown_images(text)
+        expected = []
+        self.assertEqual(images, expected)
+
+    def test_extract_markdown_links(self):
+        text = "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)"
+        links = Utils.extract_markdown_links(text)
+        expected = [("to boot dev", "https://www.boot.dev"), ("to youtube", "https://www.youtube.com/@bootdotdev")]
+        self.assertEqual(links, expected)
+
+    def test_extract_markdown_links_no_valid_links(self):
+        text = "This is text with a link ![to boot dev](https://www.boot.dev) and (https://www.youtube.com/@bootdotdev)"
+        links = Utils.extract_markdown_links(text)
+        expected = []
+        self.assertEqual(links, expected)
+
+
 if __name__ == "__main__":
     unittest.main()
--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,7 @@
+import re
+
 from leafnode import LeafNode
+from textnode import TextNode
 
 
 class Utils(object):
@@ -6,17 +9,56 @@ class Utils(object):
     @staticmethod
     def text_node_to_html_node(text_node):
         text_type = text_node.text_type
-        if text_type is "text":
+        if text_type == "text":
             return LeafNode(None, text_node.text)
-        elif text_type is "bold":
+        elif text_type == "bold":
             return LeafNode("b", text_node.text)
-        elif text_type is "italic":
+        elif text_type == "italic":
             return LeafNode("i", text_node.text)
-        elif text_type is "code":
+        elif text_type == "code":
             return LeafNode("code", text_node.text)
-        elif text_type is "link":
+        elif text_type == "link":
             return LeafNode("a", text_node.text, {"href": ""})
-        elif text_type is "image":
+        elif text_type == "image":
             return LeafNode("img", "", {"src": "", "alt": ""})
         else:
             raise Exception("Invalid text type")
+
+    @staticmethod
+    def split_nodes_delimiter(old_nodes, delimiter, text_type):
+        new_nodes = []
+        for node in old_nodes:
+            if node.text_type != "text":
+                new_nodes.append(node)
+                continue
+
+            chunks = node.text.split(delimiter)
+            chunks_length = len(chunks)
+
+            if chunks_length == 1:
+                new_nodes.append(node)
+                continue
+
+            if chunks_length % 2 == 0:
+                raise Exception(f"No opening or closing {delimiter} found in text")
+
+            for i in range(chunks_length):
+                # since a split apart string with delimiters will always have an odd number of chunks,
+                # each odd numbered chunk will be the part that previously was delimited (i.e. surrounded by **/*/`/etc)
+                # even chunks will be the surrounding non-delimited text
+                if i % 2 == 0:
+                    new_nodes.append(TextNode(chunks[i], "text"))
+                else:
+                    new_nodes.append(TextNode(chunks[i], text_type))
+
+        return new_nodes
+
+    @staticmethod
+    def extract_markdown_images(text):
+        matches = re.findall(r"!\[(.*?)\]\((.*?)\)", text)
+        return matches
+
+    @staticmethod
+    def extract_markdown_links(text):
+        matches = re.findall(r"(?<!!)\[(.*?)\]\((.*?)\)", text)
+        return matches
